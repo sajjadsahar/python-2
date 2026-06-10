b5a9071269d2a68cd982ab3273657a6f7c27d081
Sajjad Sahar <65441@students.riphah.edu.pk>
2026-06-10 04:45:12 -0700

    changes

---
+++ for.py
@@ -0,0 +1,46 @@
+#                        for loop
+# string = "hello"
+
+# # in => membership oprater
+
+# for var in string :
+#     print(var)
+
+# string = "hello"
+
+# if "o" in string:
+#     print("o is exists in a string")
+
+# for i in range(5):
+#     print(i)
+
+#                      to check how many time one latter come in word same letter
+
+# word = "Artifacil intelligence"
+
+# count = 0
+
+# for ch in word:
+#     if(ch == "i"):
+#      count += 1
+
+# print("count of i =",count)    
+
+#                            range in python 
+
+# for i in range(1,10):
+#     print(i)
+
+# for i in range(1,10,2):
+#     print(i)    
+
+#                       print sum of n number 
+
+n  = int(input("enter number :"))
+
+sum = 0
+
+for i in range(1,n+1):
+    sum += i
+
+print("total sum is ", sum)    
---
+++ loops.py
@@ -0,0 +1,45 @@
+#                                     While loop
+# count = 1; #itrater
+
+# while (count <=5): 
+#     print("hellow world",count)
+#     count +=1    
+# print("after count",count)
+
+#                              print reverse from 5 to 1 
+# i = 5
+# while(i>=1):
+#     print(i)
+#     i -=1;
+
+
+#                            multiplication of any number
+
+# n = int(input("enter num :"))
+# i = 1;
+
+# while(i<=10):
+#     print(n*i)
+#     i += 1
+
+#                                     Break and continue
+
+# i = 1
+
+# while(i <= 10):
+#     if(i % 6  == 0):
+#         break
+#     print(i)
+#     i += 1
+
+# print("outside loop now ....")    
+
+#                         print the odd numbers
+
+i = 0
+
+while(i < 10):
+    i += 1
+    if(i % 2 == 0):
+        continue
+    print(i)
